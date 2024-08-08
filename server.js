0bae77ab09ab36363d49209f955a579b1c25902c
r-89-s <satkhanov.r@gmail.com>
2024-08-08 17:28:14 +0500

    created newBlog.ejs, editBlog.ejs, newBlog.css

--- server.js
+++ server.js
@@ -26,6 +26,14 @@ app.get('/admin', (req, res) => {
     res.render("adminProfile")
 })
 
+app.get('/new', (req, res) => {
+    res.render("newBlog")
+})
+
+app.get('/edit', (req, res) => {
+    res.render("editBlog")
+})
+
 const PORT = 8000
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
