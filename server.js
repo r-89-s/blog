2d903c90b2bb56b8919ede9b74ef66de9b21aa84
r-89-s <satkhanov.r@gmail.com>
2024-08-08 15:42:31 +0500

    created register.ejs page

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ app.get('/login', (req, res) => {
     res.render("login")
 })
 
+app.get('/register', (req, res) => {
+    res.render("register")
+})
+
 const PORT = 8000
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
