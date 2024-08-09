eaf1535bc60f40ca4b163a27554b96daed7b2de7
r-89-s <satkhanov.r@gmail.com>
2024-08-09 17:32:05 +0500

    created user js files

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const app = express()
 require('./server/config/db')
 
 app.use(express.static(__dirname + '/public'))
+app.use(express.urlencoded())
 
 app.set("view engine", "ejs")
 app.use(require('./server/pages/router'))
 app.use(require('./server/Categories/router'))
+app.use(require('./server/auth/router'))
 
 const PORT = 8000
 app.listen(PORT, () => {
---
+++ server/auth/controller.js
@@ -0,0 +1,5 @@
+const User = require('./User')
+
+const signUp = async () => {
+    
+}
