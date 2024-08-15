792f5dc656776c7b4ff9b75b0e3744c4e53a1e11
r-89-s <satkhanov.r@gmail.com>
2024-08-15 20:53:54 +0500

    created Comments.js file

---
+++ server/Comments/Comments.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const CommentSchema = new mongoose.Schema({
+    text: String,
+    blogId: {type: Schema.Types.ObjectId, ref: 'blog'},
+    authorId: {type: Schema.Types.ObjectId, ref: 'user'},
+    date: Date.now()
+})
+
+module.exports = mongoose.model('comment', CommentSchema)
